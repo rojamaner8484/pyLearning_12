ed7afdaf25d29b04b84d71278b8576b6616c60bf
LENOVO <rojamaner8484@gmail.com>
2024-06-28 21:18:48 +0530

    new prog

---
+++ Ex_27062024/Lab007.py
@@ -0,0 +1,14 @@
+#Dynamically typed
+#dynamice type- type of data that pyhton supports
+age = 65
+#floating points
+#3.14,10.00,0.00
+pi=3.14
+#string= "hi","roja","i am good person"
+name = "roja"
+#boolean= True False
+ismale=True
+print(type(age))
+print(type(pi))
+print(type(name))
+print(type(ismale))
