3973c6eacf3fb968adfe7595bdcb4a4ec732f2f5
LENOVO <rojamaner8484@gmail.com>
2024-07-06 21:19:21 +0530

    class_code

---
+++ Task_sheet1/Task_2.py
@@ -0,0 +1,5 @@
+first_number=int(input("enter the first number :"))
+second_number=int(input("enter the second number :"))
+
+print("first  number is greater than second_number" if first_number > second_number else "smaller")
+print("first number is less than secodn number" if first_number < second_number else "greater")
