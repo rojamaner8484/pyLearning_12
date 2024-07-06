c12c25ca26b5ab7765d5e17d1ff8cd9c973f3cf1
LENOVO <rojamaner8484@gmail.com>
2024-07-06 20:53:15 +0530

    class_code

---
+++ Task_sheet1/Task_1.py
@@ -0,0 +1,12 @@
+# python code for calculate the square and cube of num=2 and c=8
+num = 2
+print(num ** 2)
+c = 8
+print(c ** 2)
+print("=========================================================================================================")
+
+# for cube
+num = 2
+print(num ** 3)
+c=8
+print(c**3)
